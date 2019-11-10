6398ec8250f714df4a86da7aab49e4b9ba23c506
Tyler Weitler <tweitler@gmail.com>
2019-11-09 19:04:09 -0500

    Update animate.js

--- assets/js/animate.js
+++ assets/js/animate.js
@@ -2,14 +2,15 @@ $(document).ready(function() {
   // Check if element is scrolled into view
   $(".grid__item").addClass("animated");
   function isScrolledIntoView(elem) {
-    var docViewTop = $(window).scrollTop();
-    var docViewBottom = docViewTop + $(window).height();
+      var docViewTop = $(window).scrollTop();
+      var docViewBottom = docViewTop + $(window).height();
 
-    var elemTop = $(elem).offset().top;
-    var elemBottom = elemTop + $(elem).height();
+      var elemTop = $(elem).offset().top;
+      var elemBottom = elemTop + $(elem).height();
+      var elemBottom2 = elemTop + $(elem).height() - 10;
 
-    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-  }
+      return (((elemBottom <= docViewBottom) && (elemTop >= docViewTop))) || (((elemBottom2 <= docViewBottom) && (elemTop >= docViewTop)));
+    }
   // If element is scrolled into view, fade it in
   $(window).scroll(refreshElemScroll);
 
